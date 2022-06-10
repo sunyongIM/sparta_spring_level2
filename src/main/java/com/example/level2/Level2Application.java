6408c54f5930cc57374e98468ab34b10777334c4
imsunyong <imsunyong@naver.com>
2022-06-10 17:18:53 +0900

    initial_commit

---
+++ src/main/java/com/example/level2/Level2Application.java
@@ -0,0 +1,13 @@
+package com.example.level2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Level2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Level2Application.class, args);
+    }
+
+}
